98b2e97f5ec6d586006477619b3b03fcdb0e16df
samuraya <variskhanov@yahoo.com>
2021-02-28 00:43:10 +0700

    added readme

--- database/factories/EmployeesFactory.php
+++ database/factories/EmployeesFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Employee::class, function (Faker $faker) {
     return [
         'fullname' => $faker->name,
-        'position' => $faker->randomElement(['Senior Trouble Maker', 'Junior Problem Creator', 'Public Relations or Whatever', 'Chief Executive Chef', 'Useless Ideas Generator', 'Sarah Connor Terminator','Chief Party Organizer', 'CEO of Planet Mars']),
+        'position' => $faker->randomElement(['Senior Trouble Maker', 'Junior Problem Creator', 'Zam Direktor', 'Chief Executive Chef', 'Useless Ideas Generator', 'Master Lomaster','Chief Party Organizer', 'CEO of Planet Mars']),
         'on_vacation'=> $faker->randomElement(['1', '0']),
 
     ];
